dd671fa07f56936f7132643508256ae75b6b10af
ashortsleeves <ashortsleeves@gmail.com>
2016-08-26 11:49:48 -0400

    established layout of blog

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :posts
+  get 'posts/index'
+  get 'static_pages/contact'
   get 'static_pages/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 
-   root 'static_pages#index'
+   root 'posts#index'
 end
